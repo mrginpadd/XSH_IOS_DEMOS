02d806ffa33286a8d52bab374631f02e32adec85
徐世豪 <1755883481@qq.com>
2023-08-16 08:11:05 +0800

    [新功能]-day3-tabbar滑动切换页面

--- xsh_demos/Controllers/Tabbar/WBTabbarController.swift
+++ xsh_demos/Controllers/Tabbar/WBTabbarController.swift
@@ -12,6 +12,43 @@ import UIKit
 class WBTabbarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addGes()
+        setupViews()
+        
+    }
+
+
+    // 左滑动事件处理函数
+    @objc func handleLeftSwipe() {
+        // 在这里处理左滑动事件
+        if let vcs = viewControllers, selectedIndex < vcs.count - 1 {
+            selectedIndex += 1;
+        }
+    }
+    // 右滑动事件处理函数
+    @objc func handleRightSwipe() {
+        // 在这里处理右滑动事件
+        if selectedIndex > 0 {
+            selectedIndex -= 1;
+        }
+    }
+}
+
+extension WBTabbarController {
+    
+    func addGes() {
+        // 创建左滑手势识别器
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleLeftSwipe))
+        leftSwipeGesture.direction = .left
+        view.addGestureRecognizer(leftSwipeGesture)
+        // 创建右滑手势识别器
+        let rightSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleRightSwipe))
+        rightSwipeGesture.direction = .right
+        view.addGestureRecognizer(rightSwipeGesture)
+    }
+    func setupViews() {
+        
         view.frame = UIScreen.main.bounds
         //设置安全区
         view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
@@ -19,14 +56,7 @@ class WBTabbarController: UITabBarController {
         view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         
-        setupViews()
-        
-    }
-    
-}
-
-extension WBTabbarController {
-    func setupViews() {
+        //设置tabbarItem
         let messageVC = WBTableViewController()
         let contactsVC = WBTableViewController()
         let newsVC = WBTableViewController()
